81f152861dfd3512395863633b8538f25b04f21c
AndreyRazin <programingislife@gmail.com>
2023-01-27 23:56:32 +0500

    Added new Practic

---
+++ Practica/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Practica
+{
+    class Program
+    {
+        private static void Main()
+        {
+            string str = Console.ReadLine();
+
+            try
+            {
+                for (int i = 0; i < str.Length + 1; ++i)
+                {
+                    if (str[i] >= 65 && str[i] <= 90)
+                        Console.Write($"{Char.ToLower(str[i])}");
+                    else if (str[i] >= 97 && str[i] <= 122)
+                        Console.Write($"{Char.ToUpper(str[i])}");
+                    else Console.Write($"{str[i]}");
+                }
+            }
+            catch
+            {
+                Console.WriteLine($"\nВозникло исключение!");
+            }
+        }
+    }
+}
