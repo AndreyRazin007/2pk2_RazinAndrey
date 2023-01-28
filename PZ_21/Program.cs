ddd4404fc7665d4eaade3184fc92879d2b2264ad
AndreyRazin <programingislife@gmail.com>
2023-01-28 12:52:21 +0500

    Added PZ_21

---
+++ PZ_21/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace PZ_21
+{
+    class Program
+    {
+        private static void Main()
+        {
+            Subject subject_1 = new();
+            subject_1.PrintInfo();
+
+            Console.Write("\n\n");
+
+            Subject subject_2 = new("Programing");
+            subject_2.PrintInfo();
+
+            Console.Write("\n\n");
+
+            Subject subject_3 = new("Base Data", 350); // Я к сожалению забыл, сколько на базы данных часов выделяется, прошу простить)
+            subject_3.PrintInfo();
+
+            Console.Write("\n\n");
+
+            Subject subject_4 = new("History", 320, "Credit");
+            subject_4.PrintInfo();
+
+            Console.Write("\n\n");
+
+            Subject subject_5 = new("", "");
+            subject_5.PrintInfo();
+        }
+    }
+}
---
+++ PZ_21/Subject.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace PZ_21
+{
+    class Subject
+    {
+        private string title = "Undefined";
+        private double duration;
+        private string control;
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        public double Duration
+        {
+            get { return duration; }
+            set { duration = value; }
+        }
+
+        public string Control
+        {
+            get { return control; }
+            set { control = value; }
+        }
+
+        public void PrintInfo()
+        {
+            Console.Write($"Название предмета: {Title}\n" +
+                $"Продолжительность предмета в часах: {Duration}\n" +
+                $"Итоговое оценивание предмета - экзамен или зачет: {control}");
+        }
+
+        public Subject()
+        {
+
+        }
+
+        public Subject(string title)
+        {
+            Title = title;
+        }
+
+        public Subject(string title, double duration)
+        {
+            Title = title;
+            Duration = duration;
+        }
+
+        public Subject(string title, double duration, string control)
+        {
+            Title = title;
+            Duration = duration;
+            Control = control;
+        }
+
+        public Subject(string title, string control)
+        {
+            title = "Math";
+            Title = title;
+
+            control = "Examination";
+            Control = control;
+        }
+    }
+}
