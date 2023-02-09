2d2bae06bd3ad11412f09ba9d055692a5dd5435a
AndreyRazin007 <programingislife@gmail.com>
2023-02-09 12:54:27 +0500

    Added PZ_22

---
+++ PZ_22/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace PZ_22
+{
+    class Program
+    {
+        private static void Main()
+        {
+            Subject subject_1 = new Subject(1);
+            Subject.PrintInfo();
+        }
+    }
+}
